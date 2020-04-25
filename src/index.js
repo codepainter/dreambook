e54ac3d626a58c5c3e7269af778bea23575459b3
Abiyasa <abiyasa2410@gmail.com>
2020-04-25 18:55:47 +0700

    DB: add mongoose connection

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+const { getEnv } = require('./helpers')
+
 module.exports = async (fastify, opts) => {
   // DB CONNECT
+  fastify.register(require('./mongoose-connect').fastify, {
+    uri: getEnv('MONGODB_URI', 'mongodb://localhost:27017/test'),
+    dbName: getEnv('MONGODB_DB_NAME', 'test'),
+    environment: getEnv('NODE_ENV', 'local')
+  })
 
+  // APP V1
+  fastify.register(require('./v1'))
 }
